32f219a932a659c787753b2a343a468a3327ee61
liuhang6666 <liuhang@oregonstate.edu>
2023-11-09 16:06:52 +0100

    create route function: add new user functionality in User Management Section

--- software_quiz_backend/router/route.js
+++ software_quiz_backend/router/route.js
@@ -1,13 +1,11 @@
 import { Router } from "express";
-import * as controller from '../controllers/controller.js';
-
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-
 import User from '../models/userSchema.js';
 import data from "../database/data.js";
 
 const router = Router();
+
 
 
 // After successfully logged-in,  come to this main page.
@@ -19,7 +17,16 @@ router.get('/main', (req, res) => {
     res.send({
         message: "Main Page"
     })
-});
+})
+
+
+// User management page
+router.get('/user_management_page', (req, res) =>{
+    console.log("Now you are redirected to the main page")
+    res.send({
+        message: "This is User Management Page"
+    })
+})
 
 
 
@@ -28,14 +35,11 @@ router.post('/register', async (req, res) => {
     let userName = req.body.userName;
     let userEmail = req.body.userEmail;
     let userPassword = req.body.userPassword;
-
     //Hash password for security purposes
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(userPassword, salt);
-
     //Handle cases where user email is already in use
     const record = await User.findOne({ userEmail: userEmail })
-
     if (record) {
         return res.status(400).send({
             message: "Email is already in use. Please try again with a different email"
@@ -46,18 +50,14 @@ router.post('/register', async (req, res) => {
             userEmail: userEmail,
             userPassword: hashedPassword
         })
-
         const result = await user.save()
-
         // JWT Token
         const { _id } = await result.toJSON();
         const token = jwt.sign({ _id: _id }, "secret")
-
         res.cookie("jwt", token, {
             httpOnly: true,
             maxAge: 24 * 60 * 60 * 1000  //for one day
         })
-
         res.send({
             user: result,
             message: "Account Created Succesfully"
@@ -70,27 +70,22 @@ router.post('/register', async (req, res) => {
 //login functionality, after sucessfully login, it will redirect to the main page
 router.post('/login', async (req, res) => {
     const user = await User.findOne({userEmail:req.body.userEmail})
-
     //user email not found
     if(!user){
         return res.status(404).send({
             message: "User with this Email was not found"
         })
     }
-
     //compare password user used with stored hashed version of password
     if(!(await bcrypt.compare(req.body.userPassword, user.userPassword))){
         return res.status(400).send({
             message:"Password is incorrect"
         })
     }
-
     const token = jwt.sign({_id:user._id}, "secret");
-
     res.cookie("jwt", token, {
         httpOnly:true,
         maxAge: 24*60*60*1000 // for 1 day
-
     })
 
     // Attention:  res.send() and res.redirect() can not be used at the same time, if uncomment below res.send(),  redirect() does not work
@@ -100,39 +95,75 @@ router.post('/login', async (req, res) => {
     console.log("login successful")
     //  as we have defined the unified prefix "/api", so when do redirecting, don't forget to add "/api"
     res.redirect('/api/main')
-
 })
 
 
 
 
-// returns all the data of the currently logged in user
+// returns  user information concerning the currently logged in user (only return 1 user,  not all users)
 router.get('/user', async (req, res) => {
     try {
         //get the users cookie from browser if present
         const cookie = req.cookies['jwt'];
         const claims = jwt.verify(cookie, "secret");
-
         //handle incorrect token or cookie not present
         if (!claims) {
             return res.status(401).send({
                 message: "unauthenticated"
             })
         }
-
         const user = await User.findOne({ _id: claims._id });
-
         const { userPassword, ...data } = await user.toJSON();
-
         res.send(data);
-
     } catch (err) {
         return res.status(401).send({
             message: "Unauthenticated"
         })
-
     }
 })
+
+
+
+
+
+// add new user by the employer in User Management Section
+// please remember:  change the psd into hash format;  verify that user is not existed in database
+router.post('/add_new_user',  async (req,res)=> {
+    if(!req.body){
+        res.status(400).send({ message : "Content is empty!"})
+    }
+
+    let userName = req.body.userName
+    let userEmail = req.body.userEmail
+    let userPassword = req.body.userPassword
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(userPassword, salt)
+    const record = await User.findOne({ userEmail: userEmail })
+    if (record) {
+        return res.status(400).send({
+            message: "Email is already in use. Please add the user with a different email"
+        })
+    } else {
+        const user = new User({
+            userName: userName,
+            userEmail: userEmail,
+            userPassword: hashedPassword,
+        })
+
+        user.save(user)
+            .then(data => {
+                res.redirect('/api/user_management_page')
+            })
+            .catch(err =>{
+                res.status(500).send({
+                    message : err.message || "Error happened during adding user process"
+                });
+            });
+    }
+
+
+} )
 
 
 
