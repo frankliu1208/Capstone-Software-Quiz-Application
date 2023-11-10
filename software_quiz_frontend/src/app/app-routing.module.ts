423340ccc3424d8b812c82be0c57cc2361c66587
Allison Land <landallison@gmail.com>
2023-11-09 21:15:51 -0500

    quiz component and reorganize

--- software_quiz_frontend/src/app/app-routing.module.ts
+++ software_quiz_frontend/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './modules/login/login.component';
+import { HomeComponent } from './modules/home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { AdminLoginComponent } from './modules/admin-login/admin-login.component';
 
 const routes: Routes = [
 
@@ -12,7 +13,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    component: HomeComponent
+    component: AdminLoginComponent
   },
   {
     path: 'register',
--- software_quiz_frontend/src/app/app.module.ts
+++ software_quiz_frontend/src/app/app.module.ts
@@ -4,25 +4,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './modules/login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { HomeComponent } from './home/home.component';
-import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './modules/home/home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http'
+import { AdminLoginComponent } from './modules/admin-login/admin-login.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { HeaderComponent } from './core/header/header.component';
+import { AdminLandingComponent } from './modules/admin-landing/admin-landing.component';
+import { QuizComponent } from './modules/quiz/quiz.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    HomeComponent
+    HomeComponent,
+    AdminLoginComponent,
+    HeaderComponent,
+    AdminLandingComponent,
+    QuizComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatFormFieldModule,
+    MatCardModule,
+    ReactiveFormsModule
     
   ],
   providers: [],
