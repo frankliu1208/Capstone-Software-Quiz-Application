c06756159f14bf5c65854308f5138df826c5dcd6
liuhang6666 <liuhang@oregonstate.edu>
2023-11-15 16:35:49 +0100

    modify schema

--- software_quiz_backend/models/resultOverviewSchema.js
+++ software_quiz_backend/models/resultOverviewSchema.js
@@ -5,9 +5,8 @@ const { Schema } = mongoose;
 
 /** below define the rules  */
 const resultOverviewModel = new Schema({
-    // userId : { type : String },
-    userName : { type : String},
-    userEmail : { type : String},
+    userId : { type : String},
+    quizId : { type : String},
     testDate : { type : Date},
     score : { type : Number },
     timeTaken : { type : Number}
