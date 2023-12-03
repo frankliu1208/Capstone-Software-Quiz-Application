a280cd1c543fdabfeca6347181d4b8289a8481af
Luis Hernandez <ljenuin@gmail.com>
2023-12-02 21:51:45 -0500

    updated questionSchema

--- software_quiz_backend/models/questionSchema.js
+++ software_quiz_backend/models/questionSchema.js
@@ -1,14 +1,57 @@
+// import mongoose from "mongoose";
+// const { Schema } = mongoose;
+
+
+// const questionModel = new Schema({
+//     quizId: { type: String},  // this property is needed as we need to know this questions shall belong to which quiz
+    
+//     questionContent: { type : String},
+//     type: { type : String},
+//     answersItem : { type : Array, default: []},
+//     correctAnswer: { type : Array, default: [] },
+// });
+
+// export default mongoose.model('question', questionModel);  // question is the name of the collection in MongoDB database
+
+
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-
 const questionModel = new Schema({
-    quizId: { type: String},  // this property is needed as we need to know this questions shall belong to which quiz
-    // questionId: { type : String},    // we can use _id in mongodb as the questionId
-    questionContent: { type : String},
-    type: { type : String},
-    answersItem : { type : Array, default: []},
-    correctAnswer: { type : Array, default: [] },
+    quizId: { type: String },  // this property is needed as we need to know this question shall belong to which quiz
+    
+    questionContent: { type: String },
+    type: { type: String },
+    answersItem: { type: Array, default: [] },
+    correctAnswer: { type: Array, default: [] },
 });
 
-export default mongoose.model('question', questionModel);  // question is the name of the collection in MongoDB database
+questionModel.pre('save', function(next) {
+    // Validate correctAnswer based on the question type
+    switch (this.type.toLowerCase()) {
+        case 'tf':
+            if (this.correctAnswer.length !== 1) {
+                throw new Error('For True/False questions, correctAnswer should have exactly 1 element.');
+            }
+            break;
+        case 'mc':
+            if (this.correctAnswer.length !== 1) {
+                throw new Error('For Multiple Choice questions, correctAnswer should have exactly 1 element.');
+            }
+            break;
+        case 'cata':
+            if (this.answersItem.length < 1) {
+                throw new Error('For Check All That Apply questions, there should be at least 1 answer.');
+            }
+            // No validation needed for the number of correct answers
+            break;
+        case 'ff':
+            // No validation needed for free-form questions
+            break;
+        default:
+            throw new Error('Invalid question type.');
+    }
+    next();
+});
+
+export default mongoose.model('question', questionModel);
